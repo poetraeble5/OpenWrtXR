dfdb637ed04b97e45a7e7d1992f4c33214ae253c
Philipp Schuster <ps07@gmx.com>
2020-05-27 01:57:25 +0200

    ramips: backport support for Xiaomi Redmi Router AC2100 (RM2100 white)
    Backport the support for Xiaomi Redmi Router AC2100 (RM2100 white) from https://github.com/openwrt/openwrt/commit/f3792690c4f0567a8965d82898295b9d50c3bb7e
    
    Additional fixes:
    - Changed the port_mask of the WAN led to 0x01 (already tested, the previous value 0x10 wasn't working)
    - Added the amber LED of the WAN port in addition to the white LED to the LED definitions
    - The amber LED will light up if the link speed is unkonw (speed_mask '0x01') which may indicate a cabling problem
    - The white LED will light up if a link is detected and the link speed is recognized (no matter if it is 10, 100 or 1000 Mbps)
    
    Signed-off-by: Philipp Schuster <ps07@gmx.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -298,6 +298,23 @@ define Device/xiaomi_mir3g
 endef
 TARGET_DEVICES += xiaomi_mir3g
 
+define Device/xiaomi_redmi-router-ac2100
+  DTS := REDMI-ROUTER-AC2100
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 4096k
+  IMAGE_SIZE := 124416k
+  UBINIZE_OPTS := -E 5
+  IMAGES += kernel1.bin rootfs0.bin
+  IMAGE/kernel1.bin := append-kernel
+  IMAGE/rootfs0.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_TITLE := Xiaomi Redmi Router AC2100 (RM2100 white)
+  DEVICE_PACKAGES := \
+	kmod-mt7603 kmod-mt7615e wpad-basic uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_redmi-router-ac2100
+
 define Device/mt7621
   DTS := MT7621
   BLOCKSIZE := 64k
